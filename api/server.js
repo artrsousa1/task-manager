92d2291b32fd766180af40537188645c42b3d2ca
artrsousa1 <artrsousa1@gmail.com>
2024-08-28 10:10:46 -0300

    chore: change the structure of the project

---
+++ api/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./config/db');
+const router = require('./routes/router');
+const User = require('./models/Users');
+const Task = require('./models/Task');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+sequelize.sync().then(() => {
+    console.log(`[db] Connected to the database: ${process.env.DB_NAME}`);
+})
+
+app.use('/api',router);
+app.use(express.json);
+
+app.listen(port, () => {
+    console.log(`[server] Server is running at http://localhost:${port}`);
+});
+
