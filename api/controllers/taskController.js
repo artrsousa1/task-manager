289a009a9e7c98d59c9d601b0267609c96912b11
artrsousa1 <artrsousa1@gmail.com>
2024-08-28 21:34:10 -0300

    feat(api): creating new endpoints for tasks

--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -1,10 +1,10 @@
 const TaskModel = require('../models/Task');
+const TaskEnum = require('../enums/taskEnum');
 
 const getTasks = async (req,res) => {
     try {
         const tasks = await TaskModel.findAll();
         res.status(200).send({
-            success: true,
             message: 'Tasks retrieved successfully',
             data: tasks
         });
@@ -12,27 +12,86 @@ const getTasks = async (req,res) => {
         console.log(error);
         res.status(500).send({
             success: false,
-            message: 'Internal server error'
+            message: TaskEnum.INTERNAL_SERVER_ERROR,
         })
     }
-}
+};
 
 const createTask = async (req,res) => {
     try {
         const newTask = req.body;
+
+        if(!newTask.title) {
+            res.status(400).send({
+                message: TaskEnum.BAD_REQUEST,
+            });
+        }
+
         await TaskModel.create(newTask);
         res.status(201).send({
-            success: true,
-            message: 'Task created successfully',
+            message: TaskEnum.CREATED,
             data: newTask
         });
     } catch (error) {
         console.log(error);
         res.status(500).send({
-            success: false,
-            message: 'Internal server error'
+            message: TaskEnum.INTERNAL_SERVER_ERROR,
+            error: error
+        });
+    }
+};
+
+const updateTask = async (req,res) => {
+    try {
+        const taskId = req.params.id;
+        const updatedTask = req.body;
+
+        const task = await TaskModel.findByPk(taskId);
+        if(!task) {
+            res.status(404).send({
+                message: TaskEnum.NOT_FOUND,
+            });
+        }
+
+        await task.update(updatedTask);
+        res.status(200).send({
+            message: TaskEnum.UPDATED,
+            data: updatedTask
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: TaskEnum.DELETED,
+            error: error
+        })
+    }
+};
+
+const deleteTask = async (req,res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await TaskModel.findByPk(taskId);
+        
+        if(!task) {
+            res.status(404).send({
+                message: TaskEnum.NOT_FOUND,
+            });
+        }
+
+        await task.destroy();
+        res.status(200).send({
+            message: TaskEnum.DELETED,
+            data: task
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: TaskEnum.INTERNAL_SERVER_ERROR,
+            error: error
         })
     }
 }
 
-module.exports = { getTasks };
+module.exports = { getTasks, createTask, updateTask, deleteTask };
---
+++ api/enums/taskEnum.js
@@ -0,0 +1,10 @@
+const taskEnum = {
+    CREATED: 'Task created successfully.',
+    UPDATED: 'Task updated successfully.',
+    DELETED: 'Task deleted successfully.',
+    NOT_FOUND: 'Task not found.',
+    BAD_REQUEST: 'Title field is required.',
+    INTERNAL_SERVER_ERROR: 'Internal server error.'
+}
+
+module.exports = taskEnum;
