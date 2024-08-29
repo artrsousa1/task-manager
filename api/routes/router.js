082ed64e22a127898ef070622263b606e64342b4
artrsousa1 <artrsousa1@gmail.com>
2024-08-28 21:30:37 -0300

    feat(api): add user router

--- api/routes/router.js
+++ api/routes/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const taskRouter = require('./tasksRouter');
+const userRouter = require('./usersRouter');
 
 router.use('/', taskRouter);
+router.use('/', userRouter);
 
 module.exports = router;
