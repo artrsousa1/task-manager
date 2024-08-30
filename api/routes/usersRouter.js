27853ed008bc366fb6121a3d4f7e2e516705ce0d
artrsousa1 <artrsousa1@gmail.com>
2024-08-30 12:36:45 -0300

    feat(api): creating login route

--- api/routes/usersRouter.js
+++ api/routes/usersRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 router.post('/auth/register', async (req,res) => await userController.createUser(req,res));
+router.post('/auth/login', async (req,res) => await userController.loginUser(req,res));
 
 module.exports = router;
