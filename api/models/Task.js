588fe69eae499d4c0a92e2e9e41ef32d8ca3f09f
artrsousa1 <artrsousa1@gmail.com>
2024-09-05 11:10:55 -0300

    fix: updating models to use sequelize properly

--- api/models/Task.js
+++ api/models/Task.js
@@ -1,20 +1,18 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const Task = sequelize.define('Task', {
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    status: {
-        type: DataTypes.ENUM('pending', 'in_progress', 'done'),
-        defaultValue: 'pending',
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    }
-    // Adicionar relação com usuário depois
-});
-
-module.exports = Task;
+module.exports = (sequelize, DataTypes) => {
+    const Task = sequelize.define('Task', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.ENUM('pending', 'in_progress', 'done'),
+            defaultValue: 'pending',
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        }
+    });
+    
+    return Task;
+}
--- api/models/Users.js
+++ api/models/Users.js
@@ -1,34 +1,33 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
 
-const User = sequelize.define('User', {
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-});
+    User.associate = (models) => {
+        User.hasMany(models.Task, {
+            onDelete: 'CASCADE',
+        })
+    }
 
-User.associate = (models) => {
-    User.hasMany(models.Task, {
-        onDelete: 'CASCADE',
-    })
+    return User;
 }
-
-module.exports = User;
