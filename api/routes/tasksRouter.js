bc58795e049b366cc015f934382e63ca5b54b26b
artrsousa1 <artrsousa1@gmail.com>
2024-09-05 11:11:38 -0300

    feat(api): passing jwt middleware to task routes

--- api/routes/tasksRouter.js
+++ api/routes/tasksRouter.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const taskController = require('../controllers/taskController');
+const { authenticateToken } = require('../services/authToken');
 
-router.get('/task', async (req, res) => await taskController.getTasks(req, res));
-router.post('/task', async (req, res) => await taskController.createTask(req, res));
-router.put('/update_task/:id', async (req, res) => await taskController.updateTask(req,res));
-router.delete('/delete_task/:id', async (req,res) => await taskController.deleteTask(req,res));
+router.get('/task', authenticateToken, async (req, res) => await taskController.getTasks(req, res));
+router.post('/task', authenticateToken, async (req, res) => await taskController.createTask(req, res));
+router.put('/update_task/:id', authenticateToken, async (req, res) => await taskController.updateTask(req,res));
+router.delete('/delete_task/:id', authenticateToken, async (req,res) => await taskController.deleteTask(req,res));
 
 module.exports = router;
