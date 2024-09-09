731dce021d48902b1540b90d3d973f68c3d02f4a
artrsousa1 <artrsousa1@gmail.com>
2024-09-09 13:19:58 -0300

    devops: creating test env config

--- api/config/config.js
+++ api/config/config.js
@@ -9,10 +9,10 @@ const config = {
     dialect: "mysql"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
     dialect: "mysql"
   },
   production: {
