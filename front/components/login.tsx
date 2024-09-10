4fa5bbd3a66dc4a1b95cf76528c7d567dcaa0e8c
artrsousa1 <artrsousa1@gmail.com>
2024-09-10 01:58:17 -0300

    front: add new components

--- front/components/login.tsx
+++ front/components/login.tsx
@@ -1,55 +1,75 @@
 "use client"
-
-import { Button } from "../components/ui/button"
-import {
-  Dialog,
+import { useContext } from "react";
+import { UserContext } from "@/contexts/UserContext";
+import { Button } from "../components/ui/button";
+import { Dialog,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "../components/ui/dialog"
-import { Input } from "../components/ui/input"
-import { Label } from "../components/ui/label"
+} from "../components/ui/dialog";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
 import { useState } from "react";
+import request from "@/app/utils/request";
+import { settings } from "@/app/utils/settings";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
-    const [username, setUsername] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+  const { setUser } = useContext(UserContext);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
 
-    return (
-        <Dialog>
+  async function handleLogin(username: string, password: string) {
+    const body = {
+      username: username,
+      password: password
+    }
+    const response = await request.post('/auth/login', body, settings);
+    if(response.status === 200) {
+      const { email, firstName, accessToken }  = response.data.user;
+      setUser({ email, first_name: firstName, access: accessToken });
+      router.push("/home");
+    } else {
+      console.log("Erro ao realizar login!");
+    }
+  }
+
+  return (
+    <Dialog>
       <DialogTrigger asChild>
         <Button>Log in</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Fazer login</DialogTitle>
+          <DialogTitle>Login into your account</DialogTitle>
           <DialogDescription>
-            Insira abaixo suas credenciais para acessar sua conta.
+            Insert your credentials below to access your account.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
-                Username
+              Username
             </Label>
-            <Input type="username" onChange={(e) => {setUsername(e.target.value)}} className="col-span-3" />
+            <Input type="username" onChange={(e) => { setUsername(e.target.value) }} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Password
             </Label>
-            <Input type={showPassword ? "password" : "text"} onChange={(e) => {setPassword(e.target.value)}}className="col-span-3"/>
+            <Input type={"password"} onChange={(e) => { setPassword(e.target.value) }} className="col-span-3" />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Login</Button>
+          <Button type="submit" onClick={() => handleLogin(username, password)}>Submit</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
-    );
+  );
 }
 
 export default Login;
--- front/components/signin.tsx
+++ front/components/signin.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "../components/ui/button"
 import {
   Dialog,
@@ -10,8 +12,41 @@ import {
 } from "../components/ui/dialog"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label"
+import { useState } from "react"
+import request from "@/app/utils/request"
+import { settings } from "@/app/utils/settings"
+import { isAxiosError } from "axios"
+import toast from "react-hot-toast"
 
 const Signin = () => {
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [firstName, setFirstName] = useState<string>("");
+
+    async function handleSignin(username: string, email: string, password: string, firstName: string) {
+      const body = {
+        username: username,
+        email: email,
+        password: password,
+        firstName: firstName
+      }
+      try {
+        const response = await request.post('/user/register', body, settings);
+        if(response.status === 201) {
+          toast.success(response.data.message);
+        }
+      } catch (error: unknown) {
+        if(isAxiosError(error)) {
+          const { data } = error.response || {};
+          const errorMessage = data?.message ?? "Erro ao realizar signin!";
+          toast.error(errorMessage);
+        } else {
+          toast.error("Erro ao realizar signin!");
+        }
+      }
+    };
+
     return (
         <Dialog>
       <DialogTrigger asChild>
@@ -19,27 +54,35 @@ const Signin = () => {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Fazer login</DialogTitle>
+          <DialogTitle>Create your account</DialogTitle>
           <DialogDescription>
-            Insira abaixo suas credenciais para acessar sua conta.
+            Insert your credentials below to create your account.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
+                First name
+            </Label>
+            <Input id="name" type="text" onChange={(e) => setFirstName(e.target.value)} className="col-span-3" />
+            <Label htmlFor="name" className="text-right">
+                Email
+            </Label>
+            <Input id="name" type="text" onChange={(e) => setEmail(e.target.value)} className="col-span-3" />
+            <Label htmlFor="name" className="text-right">
                 Username
             </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <Input id="name" type="text" onChange={(e) => setUsername(e.target.value)} className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Password
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Input id="username" type="password" onChange={(e) => setPassword(e.target.value)} className="col-span-3" />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" onClick={() => {handleSignin(username,email,password,firstName)}}>Create account</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
