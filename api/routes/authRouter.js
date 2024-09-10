8aea8c2332d06c409d9a6206af5f48edb854ddff
artrsousa1 <artrsousa1@gmail.com>
2024-09-10 02:01:43 -0300

    feat(api): creating refresh and logout routes

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const authController = require('../controllers/authController')
 
 router.post('/login', async (req,res) => await authController.loginUser(req,res));
+router.post('/refresh', async (req,res) => await authController.auth(req,res));
+router.get('/logout', async (req,res) => await authController.logout(req,res));
 
 module.exports = router;
