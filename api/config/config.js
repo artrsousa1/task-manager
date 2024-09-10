32e46f3e5cc850eb4fbba5b69babe8a3310df737
artrsousa1 <artrsousa1@gmail.com>
2024-09-10 02:00:30 -0300

    fix: updating env variable on db config file

--- api/config/config.js
+++ api/config/config.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const config = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     host: process.env.DB_HOST,
     dialect: "mysql"
   },
